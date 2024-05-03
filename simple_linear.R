1ddb61dc57d6b90f5a34a912e2e2a152cbeb9bce
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-05-03 15:35:24 -0400

    removed arg no longer req by fold_ttsplit

--- simple_linear.R
+++ simple_linear.R
@@ -13,7 +13,7 @@ data = cbind(imported, fold_assn)
 metrics = c()
 for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
   
-  datasets = fold_ttsplit(data, "FloodProbability", f)
+  datasets = fold_ttsplit(data, f)
   print('datasets created')
   
   mod = lm(FloodProbability ~ ., datasets[[1]])
