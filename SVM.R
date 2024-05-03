c43fe3259ef76927545504bae642fe4fa11d8f3a
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-05-03 15:34:44 -0400

    Create SVM.R

---
+++ SVM.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(e1071)
+
+setwd('C:/Users/clark/Documents/GitHub/kaggle-floods')
+
+imported = read_csv('train.csv')
+
+
+## try regression mode on two dimensions
+
+# create data
+X <- imported %>% select(!c(id, FloodProbability))
+y <- imported$FloodProbability
+
+# estimate modFloodProbability# estimate model and predict input values
+m   <- svm(x, y)
+new <- predict(m, x)
+
+# visualize
+plot(x, y)
+points(x, log(x), col = 2)
+points(x, new, col = 4)
+
+## density-estimation
+
+# create 2-dim. normal with rho=0:
+X <- data.frame(a = rnorm(1000), b = rnorm(1000))
+attach(X)
+
+# traditional way:
+m <- svm(X, gamma = 0.1)
+
+# formula interface:
+m <- svm(~., data = X, gamma = 0.1)
+# or:
+m <- svm(~ a + b, gamma = 0.1)
+
+# test:
+newdata <- data.frame(a = c(0, 4), b = c(0, 4))
+predict (m, newdata)
+
+# visualize:
+plot(X, col = 1:1000 %in% m$index + 1, xlim = c(-5,5), ylim=c(-5,5))
+points(newdata, pch = "+", col = 2, cex = 5)
