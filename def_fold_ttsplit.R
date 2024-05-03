15443050a9cfc144f3012ba1a8f59930536efc4d
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-05-03 15:33:48 -0400

    removed unused parameter

--- def_fold_ttsplit.R
+++ def_fold_ttsplit.R
@@ -1,4 +1,4 @@
-fold_ttsplit = function(prepped_data, outcome_column, fold){
+fold_ttsplit = function(prepped_data, fold){
   
   train  <- prepped_data %>% filter(fold_assn == fold) %>% select(!fold_assn)
   print("train created")
